7af6be1824fe5336e1cbb29b910def5295876e0f
molianglcl <molianglcl@163.com>
2016-05-30 21:37:11 +0800

    add Objective-c File

---
+++ zhiHuDailyWithSwift/Lib/ParallaxHeaderView/ParallaxHeaderView.swift
@@ -0,0 +1,22 @@
+//
+//  ParallaxHeaderView.swift
+//  zhiHuDailyWithSwift
+//
+//  Created by LCL on 16/5/30.
+//  Copyright © 2016年 moliang. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxHeaderView: UIView {
+
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
